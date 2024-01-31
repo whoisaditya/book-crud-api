abd07bee2c9ad77711190b506727103d47c53025
whoisaditya <mitraaditya1357@gmail.com>
2024-01-31 17:42:54 +0530

    feat: add testThatFindOneGeneratesCorrectSQL AuthorDaoImplTests

--- src/main/java/com/example/DatabaseWebApp/dao/impl/AuthorDaoImpl.java
+++ src/main/java/com/example/DatabaseWebApp/dao/impl/AuthorDaoImpl.java
@@ -3,7 +3,11 @@ package com.example.DatabaseWebApp.dao.impl;
 import com.example.DatabaseWebApp.dao.AuthorDao;
 import com.example.DatabaseWebApp.domain.Author;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class AuthorDaoImpl implements AuthorDao {
@@ -19,7 +23,22 @@ public class AuthorDaoImpl implements AuthorDao {
     }
 
     @Override
-    public Optional findOne(Author author) {
-        return Optional.empty();
+    public Optional<Author> findOne(long authorId) {
+        List<Author> results = jdbcTemplate.query("SELECT id, name FROM authors WHERE id = ? LIMIT 1",
+                new AuthorRowMapper(), authorId);
+
+        return results.stream().findFirst();
+    }
+
+    public static class AuthorRowMapper implements RowMapper<Author> {
+
+        @Override
+        public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return Author.builder()
+                    .id(rs.getLong("id"))
+                    .name(rs.getString("name"))
+                    .age(rs.getInt("age"))
+                    .build();
+        }
     }
 }
--- src/test/java/com/example/DatabaseWebApp/dao/impl/AuthorDaoImplTests.java
+++ src/test/java/com/example/DatabaseWebApp/dao/impl/AuthorDaoImplTests.java
@@ -5,12 +5,15 @@ import com.example.DatabaseWebApp.dao.impl.AuthorDaoImpl;
 import com.example.DatabaseWebApp.domain.Author;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
@@ -36,12 +39,16 @@ public class AuthorDaoImplTests {
         verify(jdbcTemplate).update(eq("INSERT INTO authors (id, name, age) VALUES (?, ?, ?)"),
                 eq(1L), eq("Aditya Mitra"), eq(21)
         );
+    }
 
-        @Test
-        public void testThatFindOneGeneratesCorrectSQL()  {
-            underTest.findOne(1L);
+    @Test
+    public void testThatFindOneGeneratesCorrectSQL()  {
+        underTest.findOne(1L);
 
-            verify(jdbcTemplate).
-        }
+        verify(jdbcTemplate).query(
+                eq("SELECT id, name FROM authors WHERE id = ? LIMIT 1"),
+                ArgumentMatchers.<AuthorDaoImpl.AuthorRowMapper>any(),
+                eq(1L)
+        );
     }
 }
