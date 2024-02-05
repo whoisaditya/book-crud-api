77d8be1e2aecd12e6862666a84890691b5abd9ec
whoisaditya <mitraaditya1357@gmail.com>
2024-02-05 10:12:08 +0530

    feat: tests can now run independently

--- src/test/java/com/example/DatabaseWebApp/dao/impl/AuthorDaoImplIntegrationTest.java
+++ src/test/java/com/example/DatabaseWebApp/dao/impl/AuthorDaoImplIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AuthorDaoImplIntegrationTest {
 
     private AuthorDaoImpl underTest;
