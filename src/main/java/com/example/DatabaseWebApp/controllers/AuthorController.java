5164a9651e4b11246617028241a8d9bd04dcd06c
whoisaditya <mitraaditya1357@gmail.com>
2024-02-12 15:58:14 +0530

    feat: added logging for Author Controller

--- src/main/java/com/example/DatabaseWebApp/controllers/AuthorController.java
+++ src/main/java/com/example/DatabaseWebApp/controllers/AuthorController.java
@@ -5,6 +5,7 @@ import com.example.DatabaseWebApp.domain.entities.AuthorEntity;
 import com.example.DatabaseWebApp.domain.dto.AuthorDto;
 import com.example.DatabaseWebApp.mappers.Mapper;
 import com.example.DatabaseWebApp.services.AuthorService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,15 +13,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
 
-
+    Logger logger = LoggerFactory.getLogger(AuthorController.class);
 
     @Autowired
     private AuthorService authorService;
@@ -30,21 +32,34 @@ public class AuthorController {
 
     @PostMapping(path = "/")
     public ResponseEntity<AuthorDto> createAuthor(@RequestBody AuthorDto authorDto) {
+        logger.info("--> Entering createAuthor method of AuthorController class");
+
         AuthorEntity authorEntity = authorMapper.mapFrom(authorDto);
         AuthorEntity savedAuthorEntity = authorService.save(authorEntity);
+
+        logger.info("<-- Exiting createAuthor method of AuthorController class");
+
         return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
     }
 
     @GetMapping()
     public Page<AuthorDto> listAuthors(Pageable pageable) {
+        logger.info("--> Entering listAuthors method of AuthorController class");
+
         Page<AuthorEntity> authors = authorService.findAll(pageable);
+
+        logger.info("<-- Exiting listAuthors method of AuthorController class");
 
         return authors.map(authorMapper::mapTo);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<AuthorDto> getAuthor(@PathVariable("id") Long id) {
+        logger.info("--> Entering getAuthor method of AuthorController class");
+
         Optional<AuthorEntity> foundAuthorEntity = authorService.findOne(id);
+
+        logger.info("<-- Exiting getAuthor method of AuthorController class");
 
         return foundAuthorEntity.map(authorEntity -> {
             AuthorDto authorDto = authorMapper.mapTo(authorEntity);
@@ -54,7 +69,10 @@ public class AuthorController {
 
     @PutMapping("/{id}")
     public ResponseEntity<AuthorDto> fullUpdateAuthor(@PathVariable("id") Long id, @RequestBody AuthorDto authorDto) {
+        logger.info("--> Entering fullUpdateAuthor method of AuthorController class");
         if(!authorService.isExists(id)) {
+            logger.info("<-- Empty/Bad Request");
+
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
@@ -62,24 +80,38 @@ public class AuthorController {
         AuthorEntity authorEntity = authorMapper.mapFrom(authorDto);
         AuthorEntity savedAuthorEntity = authorService.save(authorEntity);
 
+        logger.info("<-- Exiting fullUpdateAuthor method of AuthorController class");
+
         return  new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<AuthorDto> partialUpdateAuthor(@PathVariable("id") Long id, @RequestBody AuthorDto authorDto) {
+        logger.info("--> Entering partialUpdateAuthor method of AuthorController class");
+
         if(!authorService.isExists(id)) {
+
+            logger.info("<-- Empty/Bad Request");
+
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         AuthorEntity authorEntity = authorMapper.mapFrom(authorDto);
         AuthorEntity updatedAuthorEntity = authorService.partialUpdate(id, authorEntity);
 
+        logger.info("<-- Exiting partialUpdateAuthor method of AuthorController class");
+
         return new ResponseEntity<>(authorMapper.mapTo(updatedAuthorEntity), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteAuthor(@PathVariable("id") Long id) {
+        logger.info("--> Entering deleteAuthor method of AuthorController class");
+
         authorService.delete(id);
+
+        logger.info("<-- Exiting deleteAuthor method of AuthorController class");
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
